d3c53f1717cd3dc98075b707bf91f49c27967165
zhb <zhb2000@outlook.com>
2022-06-23 12:18:32 +0800

    Bugfix: daily task

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 import datetime
 import sys
 import os.path
-from typing import Any, Coroutine
+from typing import Any, Awaitable, Callable
 
 sys.path.append(os.path.dirname(__file__))
 import leetcode
@@ -49,23 +49,45 @@ def extract_text(chain: MessageChain) -> str:
     return text.strip() if text is not None else ''
 
 
-def config_path() -> str:
-    return os.path.join(os.path.dirname(__file__), 'config.json')
-
-
-def load_config() -> dict[str, Any]:
-    with open(config_path(), encoding='utf-8') as file:
+def load_json(path: str):
+    with open(path, encoding='utf-8') as file:
         return json.load(file)
 
 
-def set_push(group_id: int, value: bool, config: dict[str, Any]):
-    config['group_setting'][str(group_id)]['push'] = value
-    with open(config_path(), mode='w', encoding='utf-8') as file:
-        json.dump(config, file, indent=4)
+async def sleep_until(until: datetime.datetime):
+    now = datetime.datetime.now(until.tzinfo)
+    await asyncio.sleep((until - now).total_seconds())
+
+
+async def do_everyday(time: datetime.time, async_func: Callable[..., Awaitable]):
+    now = datetime.datetime.now(time.tzinfo)
+    until = datetime.datetime(
+        year=now.year,
+        month=now.month,
+        day=now.day,
+        hour=time.hour,
+        minute=time.minute,
+        second=time.second,
+        microsecond=time.microsecond,
+        tzinfo=time.tzinfo
+    )
+    if until <= now:
+        until = until + datetime.timedelta(days=1)
+    while True:
+        await sleep_until(until)
+        await async_func()
+        until = until + datetime.timedelta(days=1)
 
 
 async def main_async():
-    config = load_config()
+    config_path = os.path.join(os.path.dirname(__file__), 'config.json')
+    config: dict[str, Any] = load_json(config_path)
+
+    def set_push(group_id: int, value: bool):
+        config['group_setting'][str(group_id)]['push'] = value
+        with open(config_path, mode='w', encoding='utf-8') as file:
+            json.dump(config, file, indent=4)
+
     bot_id = config['bot_id']
     target_groups: list[int] = [int(group_id) for group_id in config['group_setting']]
     async with MiraiApi(
@@ -74,9 +96,6 @@ async def main_async():
         config['host'],
         config['port']
     ) as mirai:
-        tasks = set()
-        loop = asyncio.get_running_loop()
-
         async def push_en_daily():
             daily_message = await query_en_daily()
             for group_id, group_setting in config['group_setting'].items():
@@ -89,39 +108,22 @@ async def main_async():
                 if group_setting['push']:
                     await mirai.send_group_message(int(group_id), daily_message)
 
-        def do_everyday(time: datetime.time, coro: Coroutine):
-            async def actual_coro():
-                now = datetime.datetime.now(time.tzinfo)
-                until = datetime.datetime(
-                    year=now.year,
-                    month=now.month,
-                    day=now.day,
-                    hour=time.hour,
-                    minute=time.minute,
-                    second=time.second,
-                    microsecond=time.microsecond,
-                    tzinfo=time.tzinfo
-                )
-                if until <= now:
-                    until = until + datetime.timedelta(days=1)
-                await asyncio.sleep((until - now).total_seconds())
-                await coro
+        tasks = set()
+        loop = asyncio.get_running_loop()
 
-            def on_task_done(task):
-                tasks.discard(task)
-                do_everyday(time, coro)  # create a new task
-
-            task = loop.create_task(actual_coro())
+        def create_everyday_task(time: datetime.time, async_func: Callable[..., Awaitable]):
+            task = loop.create_task(do_everyday(time, async_func))
             tasks.add(task)
-            task.add_done_callback(on_task_done)
+            task.add_done_callback(tasks.discard)
+            return task
 
-        do_everyday(
+        create_everyday_task(
             datetime.time(0, 5, tzinfo=datetime.timezone(datetime.timedelta(hours=0))),
-            push_en_daily()
+            push_en_daily
         )  # 00:05 (UTC+00:00)
-        do_everyday(
+        create_everyday_task(
             datetime.time(0, 5, tzinfo=datetime.timezone(datetime.timedelta(hours=8))),
-            push_cn_daily()
+            push_cn_daily
         )  # 00:05 (UTC+08:00)
 
         async for data in mirai:
@@ -141,10 +143,10 @@ async def main_async():
                     elif text == '每日一题':
                         await mirai.send_group_message(group_id, await query_cn_daily())
                     elif text == 'push on':
-                        set_push(group_id, True, config)
+                        set_push(group_id, True)
                         await mirai.send_group_message(group_id, '自动推送已开启')
                     elif text == 'push off':
-                        set_push(group_id, False, config)
+                        set_push(group_id, False)
                         await mirai.send_group_message(group_id, '自动推送已关闭')
                     elif text == '' or text == 'help' or text == '帮助':
                         await mirai.send_group_message(
